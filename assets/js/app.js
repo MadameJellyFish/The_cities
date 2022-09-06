1345b82eeea884e0ff1edb87b2cf2a32140707b9
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-09-06 10:44:51 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+(function(){
+const cities={
+    // name:'John',
+    // init:function(){
+    //     alert('hello');
+    const allCities=[],
+
+    getDom: function(){
+
+    }
+    init:function(){
+        this.getDom();
+    }
+    }
+
+cities.init();
+})();
